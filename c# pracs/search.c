4b11e4dda08447f1f9334686c1f91c0f1cefdd25
dancun48 <111135378+dancun48@users.noreply.github.com>
2025-03-25 02:07:48 +0300

    comit

---
+++ c# pracs/search.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    int n = get_int("Number: ");
+    for (int i = 0; i < 7; i++){
+        if (numbers[1] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+}
